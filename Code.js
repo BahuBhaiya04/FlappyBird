579ff7be8e0b49e7ec5c08b950e0106ce14d85fa
MuditSingh <133003333+BahuBhaiya04@users.noreply.github.com>
2026-06-20 20:19:24 +0530

    Add files via upload

--- Code.js
+++ Code.js
@@ -1,14 +1,34 @@
+//--------------------Variables declaration--------------------\\
 console.log('Code.js Imported');
 let obstacle = document.body.querySelector(".obstacle");
 let playground = document.body.querySelector(".child-background");
 // console.log(obstacle);
+let background = document.body.querySelector(".background");
+
 let i=0;
 let drone = document.body.querySelector(".drone");
+
+//To Halt All ruunning Functions as soon as game ends
+let isRunning = false;  
+
+console.log(getComputedStyle(background).width);
+
+//--------------------Obstacle Designing--------------------\\
+function obstacle_displacer(params,s) {
+    params.style.transform = `translateX(${s}px)`;
+}
+
+obstacle_displacer(obstacle)
+
+function obstacle_render(params) {
+    
+}
 
 function obstacle_generator(){
     
     console.log(i);
     i++
+
     //obstacle creation
     let new_obstacle = document.createElement('div');
     new_obstacle.setAttribute('class','obstacle')
@@ -24,58 +44,12 @@ function obstacle_generator(){
     if(i<10){
         setTimeout(obstacle_generator,3000);//call back recursive function
     }
-    
+
 }
 
 
-function obstacle_render(params) {
-    
-}
-//obstacle_generator();
-// Drone Creation 
 
-// let a = window.getComputedStyle(drone);
-// console.log(a);
-
-//function drone_booster(param) {
-
-
-    // param.style.animation = 'none';
-    // param.offsetHeight;
-    // param.style.animation = 'modi-flapper 0.5s linear forwards';
-    // drone.addEventListener('animationend',()=>{
-    //     console.log('end');
-    //     drone.style.animation = 'gravity .7s cubic-bezier(0.5, 0.03, 1, 0.49) forwards';
-    // })
-
-    //event listners stack they dont get erased unless done
-    // inside browser 'Event Target Listener Registry'
-
-    // to avoid that two ways
-    // document.removeEventListener('click', handler);[this too]
-    // document.addEventListener('click', handler, { once: true }); [heavy on light devices]
-
-
-
-    // setTimeout(() => {
-    //     drone.style.animation = 'gravity 0.7s cubic-bezier(0.5, 0.03, 1, 0.49) forwards'
-    // }, 500);
-
-    //this will not stop to overwrite the continuous click command 
-    // drone will not work well
-//}
-
-//make a complete independent listener whichh will stay and is stable
-
-// drone.addEventListener('animationend',(e)=>{
-//     drone.style.animation = 'gravity .7s cubic-bezier(0.5, 0.03, 1, 0.49) forwards';
-//     console.log(e)
-// })
-
-
-let isRunning = false;
-//better gravity
-
+//--------------------Bird Building--------------------\\
 function distance_module() {
     
     let initial_drone_position_bottom = drone.getBoundingClientRect().bottom
@@ -88,6 +62,7 @@ function distance_module() {
     // console.log('distance_module offset val',offset,initial_drone_position_bottom,initial_drone_position_top,ground, ceiling);
     return position_dataset;
 }
+
 let ceiling = playground.getBoundingClientRect().top
 let ground = playground.getBoundingClientRect().bottom//not in distance odule because no need for dynaic offset
 
@@ -97,52 +72,86 @@ let offset_ceil = distance_module()[1];
 console.log(offset_ceil);
 
 // drone.style.transform = `translateY(${offset}px)`;
+function bird_tilt(params) {
+    if(isRunning){
+        params.style.transform = `rotate(${-45}deg)`;
+    }
+    else{
+        params.style.transform = `rotate(${45}deg)`;
+    }
+}
+bird_tilt(drone)
 
-function gravity(Target,tIni) {
+
+
+//--------------------Game Physics--------------------\\
+function gravity(Target,t) {
+    
     let u = 0
     let g = 0.0007;
     console.log("value of s at start of gravity module",s);
     console.log("offset from ground ",offset);
 
-    function timer(tFin) {
-        
-        
-        let t = tFin - tIni;
-        tIni = tFin;
+    // let t = tFin - tIni;
+    //     tIni = tFin;
 
         u = u+ g*t;
         s = s+ u*t;
         
         console.log("gravity_module",s);
+
+        //Gravity halting command
         if(s>=offset ) {
             console.log("gravity cancel by gravity offset statement");
-            s = offset
+            s = offset;
+            game_loop_runner = false;
             Target.style.transform = `translateY(${s}px)`;
             return;
         }
         if(isRunning){
             console.log("gravity cancel by is running statement ");
-            
             Target.style.transform = `translateY(${s}px)`;
             return;
         }
-        Target.style.transform = `translateY(${s}px)`;
-        requestAnimationFrame(timer);
-    }
-    requestAnimationFrame(timer)
+        
+    Target.style.transform = `translateY(${s}px)`;
     return s;
-}
-//better booster
-function booster(params) {
-    if (isRunning)return;
-    isRunning = true;
+    // function timer(tFin) {
+    //     let t = tFin - tIni;
+    //     tIni = tFin;
 
+    //     u = u+ g*t;
+    //     s = s+ u*t;
+        
+    //     console.log("gravity_module",s);
+    //     if(s>=offset ) {
+    //         console.log("gravity cancel by gravity offset statement");
+    //         s = offset
+    //         Target.style.transform = `translateY(${s}px)`;
+    //         return;
+    //     }
+    //     if(isRunning){
+    //         console.log("gravity cancel by is running statement ");
+            
+    //         Target.style.transform = `translateY(${s}px)`;
+    //         return;
+    //     }
+    //     Target.style.transform = `translateY(${s}px)`;
+    //     requestAnimationFrame(timer);
+    // }
+    // requestAnimationFrame(timer)
+}
+
+//booster
+function booster(params,time) {
+    if (isRunning)return;
+
+    isRunning = true;
     let u = -1;
     let jump_cal = s-(ground*0.1);
     console.log("jump_cal",jump_cal);
 
-    function timer(time) {
-        let t = 20;
+    let t = 20; //t aint time its distancing function
 
         s += u*t;
         params.style.transform = `translateY(${s}px)`
@@ -155,54 +164,70 @@ function booster(params) {
             return;
         }
         if(s<=jump_cal){
-            
             console.log("jump_cal done return", s);
             isRunning = false
             gravity(params,time)
             return;
         }
-        
-        requestAnimationFrame(timer);
-    }
+    // function timer(time) {
+    //     let t = 20;
 
-    requestAnimationFrame(timer)
+    //     s += u*t;
+    //     params.style.transform = `translateY(${s}px)`
+    //     console.log("booster module",s);
+
+    //     //jummp calibration
+    //     if (s<=offset_ceil) {
+    //         s = offset_ceil
+    //         params.style.transform = `translateY(${s}px)`;
+    //         return;
+    //     }
+    //     if(s<=jump_cal){
+    //         console.log("jump_cal done return", s);
+    //         isRunning = false
+    //         gravity(params,time)
+    //         return;
+    //     }
+        
+    //     requestAnimationFrame(timer);
+    // }
+
+    // requestAnimationFrame(timer)
 }
 
+
+//-----------------GameLoop----------------\\
+let tIni = 0;
+let game_loop_runner = true
+function game_loop(time) {
+    
+    let tFin = time-tIni;
+    tIni = time;
+
+    gravity(drone,tFin)
+    if (game_loop_runner) {
+        requestAnimationFrame(game_loop)
+    }
+    if(game_loop_runner ==false){
+        console.log("gameloop exit");
+        return;
+    }
+}
+requestAnimationFrame(game_loop)
+
+//--------------------Input Events--------------------\\
+let first_start = true
 let s = 0;  //initial value of translate 
 document.addEventListener('click',(e)=>{
     console.log("event listner s value",s);
     console.log(" click event");
-    let gravity = false;
-    booster(drone)})
-
-
-// function tracker() {
-//     let drone_coordinates = drone.getBoundingClientRect();
-//     let playground_coordinates = playground.getBoundingClientRect();
-//     console.log('tracker info');
-//     console.log(drone_coordinates,playground_coordinates);
-// }
-
-// do {
     
-// } while ();
-
-
-// cooordinate tracking
-// function tracker(time) {
-//     requestAnimationFrame(tracker);
-// }
-// function create_tracker(params) {
-//     const obj = params.getBoundingClientRect();
-//     console.log(obj.x,obj.y);
-//     tracker();
-// }
-
-
-// function tracker_module(element){
-//     function fps_module(time) {
-//         console.log(element.getBoundingClientRect(),time);
-//         requestAnimationFrame(fps_module)
-//     }
-//     fps_module();
-// }
+    let gravity = false;
+    if (first_start == false) {
+        booster(drone)
+    }
+    if (first_start) {
+        requestAnimationFrame(game_loop)
+        first_start = false;
+    }
+})
